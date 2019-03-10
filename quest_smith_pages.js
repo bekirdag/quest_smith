965393dbac1eb8cfac25198b1b68a424d5507c98
robgithub <rob.github@jumpstation.co.uk>
2019-03-10 21:38:47 +0000

    lots of styling and 404 handler

--- quest_smith_pages.js
+++ quest_smith_pages.js
@@ -4,13 +4,19 @@ var pathTaken = "";
 var xhr = new XMLHttpRequest();
 var loadingText = "Loading...";
 var pathToPages = "https://robgithub.github.io/quest_smith/story_text/";
+var homerepo = "https://github.com/bekirdag/quest_smith";
+var storyContinues = "<h2 class=\"quest404\">The Story Continues</h2><p class=\"quest404\">This part of the tale is not yet written. Take control and compose one of the many branches for the Quest Smith adventure at <a href=\"" + homerepo + "\">Quest Smith</a></p><p class=\"quest404\">You do not need to be a coder, just edit the text files</p>";
 
 function updateText(text) {
     document.querySelector(".quest_text").innerHTML = text;
 }
 
 // Converts the plain text to HTML with line breaks and removes the "choice" lines
+// captures 404
 function formatOutput(text) {
+   if (/>404</) {
+       return storyContinues;
+   }
    formatedText = text.replace(/\n/g,"<br /><br />");
    formatedText = formatedText.replace(/<br( \/)?> +Yes.*$/,"");
    return formatedText;
