b3e699c9ed791f1bf30a580c9f43f43dca6946be
robgithub <rob.github@jumpstation.co.uk>
2019-03-10 19:10:55 +0000

    js text formating function

--- quest_smith_pages.js
+++ quest_smith_pages.js
@@ -9,12 +9,19 @@ function updateText(text) {
     document.querySelector(".quest_text").innerHTML = text;
 }
 
+// Converts the plain text to HTML with line breaks and removes the "choice" lines
+function formatOutput(text) {
+   formatedText = text.replace(/\n/g,"<br /><br />");
+   formatedText = formatedText.replace(/<br( \/)?> +Yes.*$/,"");
+   return formatedText;
+}
+
 function takePath(pathTaken) {
     updateText(loadingText);
     xhr.open("GET", pathToPages + pathTaken + ".txt");
     xhr.onload = function(){
         //console.log(xhr.responseText);
-        updateText(xhr.responseText);
+        updateText(formatOutput(xhr.responseText));
     }
     xhr.send();
 }
