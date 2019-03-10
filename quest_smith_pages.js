9d3183d9177f83fc386d6e39ac4d90f23c32d657
robgithub <rob.github@jumpstation.co.uk>
2019-03-10 18:44:49 +0000

    js expanded

--- quest_smith_pages.js
+++ quest_smith_pages.js
@@ -1,7 +1,22 @@
-console.log("coming to you live from Github PAges");
-var xhr=new XMLHttpRequest();
-xhr.open("GET","https://robgithub.github.io/quest_smith/story_text/0.txt");
-xhr.onload=function(){
-    console.log(xhr.responseText);
+console.log("coming to you live from Github Pages");
+
+var pathTaken = "";
+var xhr = new XMLHttpRequest();
+var loadingText = "Loading...";
+var pathToPages = "https://robgithub.github.io/quest_smith/story_text/0";
+
+updateText(text) {
+    document.querySelector(".quest_text").innerHTML(text);
 }
-xhr.send();
+
+takePath(pathTaken) {
+    updateText(loadingText);
+    xhr.open("GET", pathToPages + pathTaken + ".txt");
+    xhr.onload = function(){
+        //console.log(xhr.responseText);
+        updateText(xhr.responseText);
+    }
+    xhr.send();
+}
+
+
